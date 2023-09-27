739b4d4bd408da33c40f4208185019b4084dcd20
UlrikeWerner <ulrike_werner@web.de>
2023-09-27 17:54:26 +0200

    css fun

--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -20,13 +20,16 @@ let blogPostList =
         },
         {
             id: 2,
-            title: "Affogato - Trendkaffee im Sommer!",
+            title: "Kaffee Cocktail - Kaffee der Untoten",
             img: {
-                src: "https://media.cdn.kaufland.de/cms/original/7b317b7519ae12c67cc808b447f30196.png",
-                alt: "Affogato"
+                src: "https://cdn.shopify.com/s/files/1/0040/7089/9776/files/Kaffee_der_Untoten_600x600.png?v=1634796944",
+                alt: "Kaffee Cocktail - Kaffee der Untoten"
             },
-            content: "Einen Espresso mit dem gewissen Extra genießen? Zartes Vanilleeis umhüllt von aromatisch-intensiven Espresso - das italienische Dessert mit einer Extraportion Koffein, Erfrischung und einem süßen Touch. Wovon wir sprechen? Natürlich vom Affogato! Wir bei roastmarket haben den Kaffeetrend natürlich auch ausprobiert und sind begeistert vom Ergebnis! Wie Sie die das leckere Affogato Rezept ganz einfach selbst Zuhause zubereiten oder Affogato gleich für mehrere Gäste servieren können, stellen wir heute vor.",
-            autor: "Max Mayer"
+            content: [
+                "Zutaten: 30ml Whisky, 30ml Brandy, 15ml Tequilla, etwas Zuckersirup, 1 Zitrone, 1 Tasse heißen Kaffee, ggf. 3EL steife Schlagsahne",
+                "Alle Zutaten (bis auf die Zitrone) mit dem heißen Kaffee mischen. Die Mischung in ein Glas füllen, etwas Zitronenfleisch herausnehmen und in das Glas geben, damit es frei schwimmend zu sehen ist. Unsere Empfehlung: Sahnehäubchen drauf geben und fertig ist der perfekte Halloween Cocktail!"
+            ],
+            autor: "Mia Klaus"
         },
 ]
 
--- src/blog/components/BlogEntry.tsx
+++ src/blog/components/BlogEntry.tsx
@@ -8,16 +8,24 @@ type BlogEntryProps = {
         src: string,
         alt: string
     },
-    content: string,
+    content: string | string[],
     autor: string
 }
 export default function BlogEntry(props: BlogEntryProps){
     return(
-        <section>
-            <h3>{props.title}</h3>
-            <img className="postImage" src={props.img.src} alt={props.img.alt}/>
-            <textarea>{props.content}</textarea>
-            <p>{props.autor}</p>
+        <section className="text-left blogEntryButton">
+            <p className="blogEntryAutor">author: {props.autor}</p>
+            <h3 className="blogEntry-title">{props.title}</h3>
+                <div className="blogEntry-flex-row">
+                    <img className="postImage" src={props.img.src} alt={props.img.alt}/>
+                    <div className="blogEntry-content-flex">
+                        {
+                            (typeof props.content === "string") ?
+                                <p className="text-style">{props.content}</p> :
+                                props.content.map((paragraph) => <p className="text-style">{paragraph}</p>)
+                        }
+                    </div>
+                </div>
         </section>
     )
 }
